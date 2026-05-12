285133b48bfa47b42a4fe566a80cbda67634e5a8
designerwee <designerwee@gmail.com>
2026-05-12 20:30:00 +0900

    refactor: Button.astro, Footer.astro, Header.astro 코드개선 및 컴포넌트 추가

---
+++ src/types/button.ts
@@ -0,0 +1,10 @@
+export interface ClassList {
+   type?: 'btn-xs' | 'btn-sm' | 'btn-base' | 'btn-lg' | 'btn-xl';
+   bgColor?: string;
+   fontColor?: string;
+   borderColor?: string;
+   hoverBgColor?: string;
+   hoverFontColor?: string;
+   hoverBorderColor?: string;
+   fontWeight?: string;
+}
